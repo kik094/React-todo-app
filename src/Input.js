b79a359a11c915ec74b13c55e6e7766cb79d35de
kik094 <79009903+kik094@users.noreply.github.com>
2021-07-26 16:30:47 +0300

    Add files via upload

--- src/Input.js
+++ src/Input.js
@@ -3,7 +3,7 @@ import React from "react";
 const Input = ({ input, setInput, handleSubmit, dropdownHandler }) => {
   return (
     <div className='todo-heading-wrapper'>
-      <h2 className="todo-heading-wrapper-h2">Todo list</h2>
+      <h2 className="todo-heading-wrapper-h2">To do list</h2>
     <form className="form-container" onSubmit={handleSubmit}>
       <input
         value={input}
@@ -11,7 +11,7 @@ const Input = ({ input, setInput, handleSubmit, dropdownHandler }) => {
         type="text"
         placeholder="Add todo..."
       />
-      <button>Add to list</button>
+      <button className="add-btn">Add to list</button>
       <select className="dropdown" onChange={dropdownHandler}>
         <option value="all">All</option>
         <option value="completed">Completed</option>
